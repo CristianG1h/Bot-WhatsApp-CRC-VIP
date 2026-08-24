fb5c5b308b33363b46630d0e6d047d8d8a65ddb7
CristianG1h <94920291+CristianG1h@users.noreply.github.com>
2026-08-24 09:53:14 -0500

    feat: enviar información CRC antes del flujo Meta

--- src/services/whatsapp.js
+++ src/services/whatsapp.js
@@ -1,6 +1,35 @@
 const axios = require("axios");
 const { WHATSAPP_TOKEN, PHONE_NUMBER_ID } = require("../config");
 const { prepararMensajeSinConsultasExternas } = require("../utils/sessions");
+const {
+  MENSAJE_INICIAL_CRC,
+  debeEnviarMensajeInicial,
+  marcarMensajeInicialEnviado,
+} = require("../utils/welcome");
+
+function esperar(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function enviarTextoMeta(to, body) {
+  const url = `https://graph.facebook.com/v20.0/${PHONE_NUMBER_ID}/messages`;
+
+  await axios.post(
+    url,
+    {
+      messaging_product: "whatsapp",
+      to,
+      type: "text",
+      text: { body }
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${WHATSAPP_TOKEN}`,
+        "Content-Type": "application/json"
+      }
+    }
+  );
+}
 
 async function sendText(to, body) {
   const mensaje = prepararMensajeSinConsultasExternas(to, body);
@@ -10,23 +39,13 @@ async function sendText(to, body) {
     return;
   }
 
-  const url = `https://graph.facebook.com/v20.0/${PHONE_NUMBER_ID}/messages`;
+  if (debeEnviarMensajeInicial(to)) {
+    await enviarTextoMeta(to, MENSAJE_INICIAL_CRC);
+    marcarMensajeInicialEnviado(to);
+    await esperar(600);
+  }
 
-  await axios.post(
-    url,
-    {
-      messaging_product: "whatsapp",
-      to,
-      type: "text",
-      text: { body: mensaje }
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${WHATSAPP_TOKEN}`,
-        "Content-Type": "application/json"
-      }
-    }
-  );
+  await enviarTextoMeta(to, mensaje);
 }
 
 module.exports = { sendText };
